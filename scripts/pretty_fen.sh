40336fd21119e50da4e5324a8ea23e2e273d40c8
demiaus <demiaus@amideus.fi>
2024-12-05 08:34:53 +0200

    Migrate to xsel

--- scripts/pretty_fen.sh
+++ scripts/pretty_fen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-~/.local/bin/pretty_fen "$(xclip -o -selection clipboard)" | xclip -selection clipboard
+# ~/.local/bin/pretty_fen "$(xclip -o -selection clipboard)" | xclip -selection clipboard
+~/.local/bin/pretty_fen "$(xsel -o --clipboard)" | xsel -i --clipboard
 
