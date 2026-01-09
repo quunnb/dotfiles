3d83cf27ca0bbbda41edaf6e08b24b2e0415c814
quunnb <quunnb@amideus.fi>
2026-01-10 00:21:50 +0200

    Add files for gtk and anki addons

---
+++ .config/anki/addons21/1933645497/binding.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+# Handles the main integration (bindings) with Anki
+#
+# This files is part of fill-the-blanks addon
+# @author ricardo saturnino
+# -------------------------------------------------------
+instance = None
+
+import os
+
+from anki import hooks
+from anki.hooks import wrap, addHook
+from anki.utils import strip_html
+from aqt import gui_hooks
+from aqt import mw
+from aqt.reviewer import Reviewer
+from aqt.utils import tooltip
+
+from .config import ConfigService, ConfigKey
+from .handler import addon_field_filter, on_show_question, handle_answer, cleanup_context, AnkiInterface, getTypedAnswer, FieldsContext
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+
+CSS_STYLE = """
+<style type="text/css">
+input.ftb {
+    border-radius: 5px;
+    border: 1px solid;
+    min-width: 50px;
+    max-width: 400px;
+    padding: 3px;
+    margin: 2px;
+}
+input.ftb-md {
+    width: 150px;
+}
+input.st-incomplete {
+    background-color: #ffcc00;
+    color: #282420;
+}
+input.st-error {
+    background-color: #dd4400;
+    color: #282420;
+}
+input.st-ok {
+    background-color: #88bb44;
+    color: #282420;
+}
+
+</style>
+"""
+
+_handler = None
+JS_LOCATION = CWD + '/fill-blanks.js'
+_warn_template_shown = False
+
+
+def _ankiConfigRead(key):
+    return mw.addonManager.getConfig(__name__)[key]
+
+
+def warn_template_editor(*args):
+    global _warn_template_shown
+    if not _warn_template_shown:
+        tooltip("[Fill-in-the-blanks] Be aware: The add-on does not apply to the template editor. To check it, please go to Review mode", 9000)
+        _warn_template_shown = True
+
+
+def wrapInitWeb(anki_mw, fn):
+    def _initReviewerWeb(*args):
+        fn()
+
+        addStylesJs = """
+                var prStyle = `{}`;
+
+                $(prStyle).appendTo('body');
+                """.format(CSS_STYLE)
+
+        anki_mw.reviewer.web.eval(addStylesJs)
+
+        f = open(JS_LOCATION, 'r')
+        anki_mw.reviewer.web.eval("""
+                %s
+            """ % f.read())
+
+        if not ConfigService.read(ConfigKey.FEEDBACK_ENABLED, bool):
+            anki_mw.reviewer.web.eval('disableInstantFb();')
+
+        if ConfigService.read(ConfigKey.IGNORE_CASE, bool):
+            anki_mw.reviewer.web.eval('ignoreCaseOnFeedback();')
+
+        if ConfigService.read(ConfigKey.IGNORE_ACCENTS, bool):
+            anki_mw.reviewer.web.eval('ignoreAccentsOnFeedback();')
+
+        if ConfigService.read(ConfigKey.ASIAN_CHARS, bool):
+            print('Enabling experimental Asian Chars mode')
+            anki_mw.reviewer.web.eval('enableAsianChars();')
+
+    return _initReviewerWeb
+
+
+def _setup_anki_integration():
+    AnkiInterface.staticReviewer = mw.reviewer
+    AnkiInterface.strip_HTML = strip_html
+    FieldsContext.ignore_case = ConfigService.read(ConfigKey.IGNORE_CASE, bool)
+
+    hooks.field_filter.append(addon_field_filter)
+    addHook("showQuestion", on_show_question)
+
+    gui_hooks.card_layout_will_show.append(warn_template_editor)
+    gui_hooks.card_will_show.append(handle_answer)
+    gui_hooks.reviewer_did_answer_card.append(cleanup_context)
+
+    Reviewer._getTypedAnswer = wrap(Reviewer._getTypedAnswer,
+                                    lambda _, _old: getTypedAnswer(_old), "around")
+
+
+def run():
+    # tooltip('Loading fill-the-blanks Handler')
+    ConfigService.load_config = _ankiConfigRead
+
+    reviewer = mw.reviewer
+    reviewer._initWeb = wrapInitWeb(mw, reviewer._initWeb)
+
+    _setup_anki_integration()
